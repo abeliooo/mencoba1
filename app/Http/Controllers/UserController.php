2b05d67726803f392ca8eac80afa037cb27ee59c
abeliooo <stefanusabel29@gmail.com>
2025-03-17 21:57:44 +0700

    Part #3
    Bikin sarana untuk login dan register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\UserDetails;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
@@ -35,6 +36,29 @@ class UserController extends Controller
             ]);
         }
         return $user->createToken($request->email)->plainTextToken;
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'type'=>'user',
+            'password'=>Hash::make($request->password),
+        ]);
+
+        $userInfo = UserDetails::create([
+            'user_id'=>$user->id,
+            'status'=>'active',
+        ]);
+
+        return $user;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::post('/login', [UserController::class, 'login']);
-
+Route::post('/register', [UserController::class, 'register']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
